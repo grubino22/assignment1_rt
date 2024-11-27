2d13733b4423e831f6147eef21c7d0e7d5043770
Giuseppe <s7679508@studenti.unige.it>
2024-11-27 20:23:02 +0000

    Node2 update: boundary control

--- scripts/distance.py
+++ scripts/distance.py
@@ -4,6 +4,10 @@ from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
 import math
 
+treshold = 1.0;
+high_boundary = 10;
+low_boundary = 1;
+
 def turtle1Callback(msg):
 	global turtle1_pose
 	turtle1_pose = msg
@@ -17,8 +21,10 @@ def compute_distance():
 
 def control_distance():
 	
+	global turtle1_pub
 	turtle1_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-	turtle1_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+	global turtle2_pub
+	turtle2_pub = rospy.Publisher('/turtle2/cmd_vel', Twist, queue_size=10)
 	
 	rospy.Subscriber('/turtle1/pose', Pose, turtle1Callback)
 	rospy.Subscriber('/turtle2/pose', Pose, turtle2Callback)
@@ -29,7 +35,19 @@ def control_distance():
 	while not rospy.is_shutdown():
 		
 		distance = compute_distance()
-		print("Distance: ", distance)
+		if distance < treshold:
+			print("The turtles are too close!")
+			turtle1_pub.publish(Twist())
+			turtle2_pub.publish(Twist())
+			
+		if (turtle1_pose.x <= low_boundary or turtle1_pose.y <= low_boundary or turtle1_pose.x >= high_boundary or turtle1_pose.y >= high_boundary):
+			print("turtle1 is too close to the boundary, it's being stopped")
+			turtle1_pub.publish(Twist())
+			
+		if (turtle2_pose.x <= low_boundary or turtle2_pose.y <= low_boundary or turtle2_pose.x >= high_boundary or turtle2_pose.y >= high_boundary):
+			print("turtle2 is too close to the boundary, it's being stopped")
+			turtle2_pub.publish(Twist())
+		
 		
 			
 		rate.sleep()
